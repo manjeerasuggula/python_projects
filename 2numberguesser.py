7b14530dda63418699949d1385c1d54e0bd45b99
manjeera <130635674+manjeerasuggula@users.noreply.github.com>
2024-03-14 14:37:04 +0530

    Add files via upload

---
+++ 2numberguesser.py
@@ -0,0 +1,35 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("please type a number larger than 0 next time")
+        quit()
+else:
+    print("please enter a number")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+# print(random_number)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("please type a number next time")
+        continue
+
+    if user_guess == random_number:
+        print("you got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number")
+    else:
+        print("You were below the number")
+print("you got it in", guesses, "guesses")
